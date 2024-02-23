75b51aa8f8341215ecce8e39c60ed704408d98ec
deniseschirmer <deniseschirmerr@gmail.com>
2024-02-23 08:14:19 -0300

    setting struct

---
+++ src/services/user/CreateUserService.ts
@@ -0,0 +1,9 @@
+import { Request, response, Response } from "express";
+
+class CreateUserController {
+  async handle(req: Request, res: Response) {
+    return res.json({ ok: true });
+  }
+}
+
+export { CreateUserController };
