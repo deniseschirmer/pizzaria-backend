f2fc3809bb1df62fd5a344c7737bc6f6359f7183
deniseschirmer <deniseschirmerr@gmail.com>
2024-02-29 11:47:43 -0300

    feat: adding middleware and add user details retrieval/category registration

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,23 @@ import { Router } from "express";
 
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 
 const router = Router();
 
 //-- ROTAS USER --
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
+
+router.get("/me", isAuthenticated, new DetailUserController().handle);
+
+//ROTAS CATEGORY
+router.post(
+  "/category",
+  isAuthenticated,
+  new CreateCategoryController().handle
+);
 
 export { router };
