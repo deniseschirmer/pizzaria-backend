ccc50767b00504f1090c57db824e1149f6a9bdb1
deniseschirmer <deniseschirmerr@gmail.com>
2024-03-04 10:34:48 -0300

    feat: listing category

--- src/routes.ts
+++ src/routes.ts
@@ -5,7 +5,7 @@ import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
-
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
 const router = Router();
 
 //-- ROTAS USER --
@@ -20,5 +20,6 @@ router.post(
   isAuthenticated,
   new CreateCategoryController().handle
 );
+router.get("/category", isAuthenticated, new ListCategoryController().handle);
 
 export { router };
