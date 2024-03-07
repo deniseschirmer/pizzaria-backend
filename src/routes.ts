fa4335a8666bf08610f1266205bfd17ac264c0d5
deniseschirmer <deniseschirmerr@gmail.com>
2024-03-07 10:19:01 -0300

    feat: Creating and deleting order

--- src/routes.ts
+++ src/routes.ts
@@ -3,11 +3,18 @@ import multer from "multer";
 
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
+
 import { DetailUserController } from "./controllers/user/DetailUserController";
-import { isAuthenticated } from "./middlewares/isAuthenticated";
+
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
+
 import { CreateProductController } from "./controllers/product/CreateProductController";
+import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
+import { CreateOrderController } from "./controllers/order/CreateOrderController";
+import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
+
+import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 import upLoadConfig from "./config/multer";
 
@@ -36,5 +43,13 @@ router.post(
   upload.single("file"),
   new CreateProductController().handle
 );
+router.get(
+  "/category/product",
+  isAuthenticated,
+  new ListByCategoryController().handle
+);
 
+//-- Rotas ORDER
+router.post("/order", isAuthenticated, new CreateOrderController().handle);
+router.delete("/order", isAuthenticated, new RemoveOrderController().handle);
 export { router };
